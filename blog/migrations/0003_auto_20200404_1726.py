1ab721b20153ce5b9ceefdc806ab08fdd210e0f4
Prashant Mishra <2017188@iiitdmj.ac.in>
2020-04-26 00:40:14 +0530

    updated migrations files

--- blog/migrations/0003_auto_20200404_1726.py
+++ blog/migrations/0003_auto_20200404_1726.py
@@ -16,6 +16,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='postcomments',
             name='authors',
-            field=models.ForeignKey(default='noob', on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
     ]
